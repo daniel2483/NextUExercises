d6e9c0b27ef701b675f531b390d2e1586c7e12c8
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-09-20 18:30:49 -0600

    Adding new files

--- DesarrolloWeb/Modulo_6/Unidad 3 - PHP/Ejemplo-Ejercicio 7 - Manejo_formularios/recepcion_formulario.php
+++ DesarrolloWeb/Modulo_6/Unidad 3 - PHP/Ejemplo-Ejercicio 7 - Manejo_formularios/recepcion_formulario.php
@@ -1,5 +1,11 @@
 <?php
 
-  echo "<h1>Se han recibido los datos del formulario exitosamente</h1>";
+  //echo "<h1>Se han recibido los datos del formulario exitosamente</h1>";
+
+  //$nombre = $_GET['nombre_usuario']; // --> Con metodo GET
+  //$nombre = $_POST['nombre_usuario']; // --> Con metodo POST
+  $nombre = $_POST['nombre']; // --> Con AJAX y método POST
+  $numeroLetras = strlen($nombre);
+  echo "El nombre recibido del formulario es: ".$nombre." y tiene ".$numeroLetras." letras";
 
  ?>
