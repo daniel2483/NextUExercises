d7e3167faa6fc45979d4fbd7990c6ed1f67f6246
Daniel R <jdrs2483@gmail.com>
2020-09-22 18:52:48 -0600

    Ejercicio - Tarea 3 - Formularios

---
+++ DesarrolloWeb/Modulo_6/Unidad 3 - PHP/Tarea 3 - Formularios/ejercicio/js/descripcion.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('select').material_select();
+
+    $('.preloader-wrapper').hide()
+    $( document ).ajaxStart(function() {
+      $( ".preloader-wrapper" ).show();
+    });
+    $( document ).ajaxStop(function() {
+      $( ".preloader-wrapper" ).hide();
+    });
+
+    // Código para el uso de AJAX para envio de datos de un form POST
+    $('.desc-form').submit(function(event){
+      //console.log("TEST")
+      var categoria = $("select#categoria").children("option:selected").val();
+      var descripcion = $.trim($('#descripcion').val());
+      console.log("Categoria: "+categoria);
+      console.log("Descripción: "+descripcion);
+
+      event.preventDefault(); // Para evitar que el formulario se envíe por defecto
+
+      //$.ajax(
+      //  {
+      //    url:'./descripcion.php',
+      //    type:'post',
+      //    data: {user:user1, password:password1}
+      //  }
+      //).done(function(data){
+      //  alert(data);
+      //})
+    })
+
+
+
+});
