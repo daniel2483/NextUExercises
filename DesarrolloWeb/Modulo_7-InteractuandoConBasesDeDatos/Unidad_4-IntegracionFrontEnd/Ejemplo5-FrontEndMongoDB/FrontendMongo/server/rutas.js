7a65e233a9459038db8831b8e1e12ca3fa457893
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2020-10-30 13:16:56 -0600

    Desarrollo Web - Module 7 - Unit 4 - Lesson 3

---
+++ DesarrolloWeb/Modulo_7-InteractuandoConBasesDeDatos/Unidad_4-IntegracionFrontEnd/Ejemplo5-FrontEndMongoDB/FrontendMongo/server/rutas.js
@@ -0,0 +1,27 @@
+const Router = require('express').Router();
+
+Router.get('all', function (req, res){
+
+})
+
+Router.get('/:id', function (req, res){
+
+})
+
+Router.post('/new', function (req, res){
+
+})
+
+Router.post('/delete/:id', function (req, res){
+
+})
+
+Router.post('/inactive/:id', function (req, res){
+
+})
+
+Router.post('/active/:id', function (req, res){
+
+})
+
+module.exports = Router
