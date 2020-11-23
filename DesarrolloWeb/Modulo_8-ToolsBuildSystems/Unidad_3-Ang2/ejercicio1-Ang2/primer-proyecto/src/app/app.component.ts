4ee87229e654ecb6d6712ce71f7aa207c513568c
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2020-11-23 17:03:24 -0600

    Module 8 - Unit 3 - Lesson 2 - Started

--- DesarrolloWeb/Modulo_8-ToolsBuildSystems/Unidad_3-Ang2/ejercicio1-Ang2/primer-proyecto/src/app/app.component.ts
+++ DesarrolloWeb/Modulo_8-ToolsBuildSystems/Unidad_3-Ang2/ejercicio1-Ang2/primer-proyecto/src/app/app.component.ts
@@ -1,9 +1,12 @@
+// Importa el modulo componente del core de angular
 import { Component } from '@angular/core';
+import { BarraSuperiorComponent } from './barra-superior/barra-superior.component'
 
+// Decorador, acciones en segundo plano
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // Identificador del componente, y sera llamado como una etiqueta HTML
+  templateUrl: './app.component.html', // Vista del componente
+  styleUrls: ['./app.component.css'] // Estilos para el componente en cuestion
 })
 export class AppComponent {
   title = 'Hola Mundo!!';
