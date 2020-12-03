ce1cb32bec480e44d2e4bc2b5e577d3d25b47282
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2020-12-03 13:46:29 -0600

    Desarrollo Web - Module 8 - Unit 3 - Lesson 4 - Completed

---
+++ DesarrolloWeb/Modulo_8-ToolsBuildSystems/Unidad_3-Ang2/ejemplo7-Get/src/app/http.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
+
+@Injectable()
+export class HttpService {
+
+  constructor(private http : Http) { }
+
+  getDatos(){
+    return this.http.get('https://ejemplo-http.firebaseio.com/.json')
+      .map((response: Response)=> response.json())
+  }
+}
