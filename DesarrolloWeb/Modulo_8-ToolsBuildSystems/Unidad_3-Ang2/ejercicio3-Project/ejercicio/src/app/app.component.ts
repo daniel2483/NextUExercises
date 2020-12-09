dc9226ab2f0aeec591690cea54dd4d6e4743e316
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2020-12-09 11:26:02 -0600

    Desarrollo Web - Module 8 - Unit 3 - Completed

---
+++ DesarrolloWeb/Modulo_8-ToolsBuildSystems/Unidad_3-Ang2/ejercicio3-Project/ejercicio/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BarraSuperiorComponent } from './barra-superior/barra-superior.component';
+import { InicioComponent } from './inicio/inicio.component';
+//import { NuevaTareaComponent } from './nueva-tarea/nueva-tarea.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ejercicio';
+}
---
+++ DesarrolloWeb/Modulo_8-ToolsBuildSystems/Unidad_3-Ang2/ejercicio3-Project/ejercicio/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { BarraSuperiorComponent } from './barra-superior/barra-superior.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    BarraSuperiorComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
