f0a8c0e545fed29208fff987efdfd76ef44d11c0
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2020-12-23 10:07:04 -0600

    Desarrollo Web - Module 8 - Unit 4 - Lesson 2 - Finished

--- DesarrolloWeb/Modulo_8-ToolsBuildSystems/Unidad_4-ReactJS/Ejemplo 1 - ReactEx/reactapp/firstapp/src/App.js
+++ DesarrolloWeb/Modulo_8-ToolsBuildSystems/Unidad_4-ReactJS/Ejemplo 1 - ReactEx/reactapp/firstapp/src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>First App</h1>
+        <p>Hello World React!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
