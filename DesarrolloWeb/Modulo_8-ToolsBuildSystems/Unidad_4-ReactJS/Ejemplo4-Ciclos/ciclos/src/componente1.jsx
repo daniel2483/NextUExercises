3876aad8fc484a2a1a9856451d77368d94003212
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2020-12-25 15:32:44 -0600

    Desarrollo Web - Module 8 - Unit 4 - Lesson 4 - continuing

--- DesarrolloWeb/Modulo_8-ToolsBuildSystems/Unidad_4-ReactJS/Ejemplo4-Ciclos/ciclos/src/componente1.jsx
+++ DesarrolloWeb/Modulo_8-ToolsBuildSystems/Unidad_4-ReactJS/Ejemplo4-Ciclos/ciclos/src/componente1.jsx
@@ -5,24 +5,35 @@ class Componente1 extends React.Component{
   constructor(){
     super()
     this.state = {
-      mensaje: 'Bienvenido a ReactJS con Jose Daniel Rodriguez',
-      mensaje2: 'Learning States & Props'
+      bienvenido: 'Bienvenido al método constructor'
     }
+    console.log('Primera fase: Método Constructor')
 
   }
   render(){
+    console.log('Tercera fase: Método Render, fase principal')
+    var valor1 = 500;
+    var valor2 = 450;
+    var producto = valor1 * valor2;
     return(
       <div>
-        <h1>{this.state.mensaje}</h1>
-        <Componente2 mensajeProps={this.state.mensaje2} cambioEstado={this.changeState.bind(this)}/>
-        <div>
-          <button onClick={this.changeState.bind(this)}>Cambiar Estado</button>
-        </div>
+        <h1>{this.state.bienvenido}</h1>
+        <h3>El valor de multiplicación es: {producto}</h3>
       </div>
     )}
 
-  changeState(){
-    this.setState({ mensaje: 'Este es un nuevo mensaje...'})
+  //changeState(){
+  //  this.setState({ mensaje: 'Este es un nuevo mensaje...'})
+  //}
+
+  componentWillMount(){
+    console.log('Bienvenido al metodo componentWillMount');
+    console.log('Segunda fase: Método componentWillMount');
+  }
+
+  componentDidMount(){
+    console.log('Cuarta fase: Método componentDidMount, última fase del ciclo de montaje');
+    this.setState({bienvenido:'Bienvenido al método componentDidMount'})
   }
 
 }
