2aabc39d220e2e62257a490ed09b5b7523f2f41d
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2020-12-29 20:05:08 -0600

    Desarrollo Web - Module 8 - Unit 4 - Lesson 5 - Starting

---
+++ DesarrolloWeb/Modulo_8-ToolsBuildSystems/Unidad_4-ReactJS/Ejemplo5-Router/router/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Usuarios</li>
+          <li>Lenguajes</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App;
